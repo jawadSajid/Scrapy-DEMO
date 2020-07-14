e18456df272a054dc4aa0c8b763d25968d0e930a
Jawad Sajid <jawadsajid96@gmail.com>
2020-07-14 23:28:38 +0500

    Scraped data in files, work on pipelines to store in sqllite3

---
+++ quotes/database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("myquotes.db")
+curr = conn.cursor()
+
+# curr.execute("""create table quotes_tb(
+#                 title text,
+#                 author text,
+#                 tag text
+#                 )""")
+
+# curr.execute("""insert into quotes_tb values(
+#                 "Python is awesome" , "Jawad" , "PP"
+#                 )""")
+
+conn.commit()
+conn.close()
