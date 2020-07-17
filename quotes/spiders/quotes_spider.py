3fda7c395ef0673efc53bac5bf541f4319aec414
Jawad Sajid <jawadsajid96@gmail.com>
2020-07-18 04:28:22 +0500

    FormRequest login and then scrape added!

--- quotes/spiders/quotes_spider.py
+++ quotes/spiders/quotes_spider.py
@@ -1,18 +1,34 @@
 import scrapy
 from ..items import QuotesItem
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    page_number = 2
+    # page_number = 2
+    # start_urls = [
+    #     "http://quotes.toscrape.com/page/1/"
+    # ]
     start_urls = [
-        "http://quotes.toscrape.com/page/1/"
+        "http://quotes.toscrape.com/login"
     ]
 
     def parse(self, response):
         # title = response.css("title::text").extract()
         # yield {'titletext': title}
+        token = response.css("form input::attr(value)").extract_first()
+        print("Token: " + token)
+        return FormRequest.from_response(response, formdata={
+            'csrf_token': token,
+            'username': 'jawad@gmail.com',
+            'password': 'wallahi123'
+        }, callback=self.start_scraping)
 
+    def start_scraping(self, response):
+        open_in_browser(response)
+
+        # Extracting title, author and quotes
         items = QuotesItem()
 
         all_div_quotes = response.css("div.quote")
@@ -40,8 +56,8 @@ class QuotesSpider(scrapy.Spider):
         #     yield response.follow(next_page, callback=self.parse)
 
         # Pagination
-        next_page = "http://quotes.toscrape.com/page/" + str(QuotesSpider.page_number) + "/"
-        print(next_page)
-        if QuotesSpider.page_number < 11:
-            QuotesSpider.page_number += 1
-            yield response.follow(next_page, callback=self.parse)
+        # next_page = "http://quotes.toscrape.com/page/" + str(QuotesSpider.page_number) + "/"
+        # print(next_page)
+        # if QuotesSpider.page_number < 11:
+        #     QuotesSpider.page_number += 1
+        #     yield response.follow(next_page, callback=self.parse)
